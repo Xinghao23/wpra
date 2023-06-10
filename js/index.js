c7171ae14463ca1d176f69f5fe60be87136e844b
battleoh <denny2002zeng@gmail.com>
2023-06-10 15:11:53 -0700

    main page nav for mobile update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.target-burger').addEventListener('click', () => {
+        document.querySelector('body').classList.toggle('toggled');
+    })
+})
