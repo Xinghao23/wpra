d05bfb4ced33292ca0a2d70e68d1cc9f95237087
xinghao23 <xinghao23@hotmail.com>
2023-06-29 23:48:25 -0700

    added some magic

--- js/common.js
+++ js/common.js
@@ -42,3 +42,18 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+//Automatic Slide run though 
+/*
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}*/
