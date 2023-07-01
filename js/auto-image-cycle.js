a58babc815e8205ff929db060ccbf2d20ac46d9d
Stanley <sty447@gmail.com>
2023-07-01 10:09:12 -0700

    add program  subpage

--- js/auto-image-cycle.js
+++ js/auto-image-cycle.js
@@ -1,12 +1,41 @@
+
 //auto image cycle
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+let slideIndex = 0;
+let timeoutId = null;
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
+
+showSlides();
+function currentSlide(index) {
+  slideIndex = index;
+  showSlides();
+}
+function plusSlides(step) {
+
+  if (step < 0) {
+    slideIndex -= 2;
+
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 5000); // Change image every 5 seconds
+
+  showSlides();
+}
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove('active');
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add('active');
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
